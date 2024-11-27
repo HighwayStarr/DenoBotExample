5fe82b7459e729208234be9ef22659d93262deb4
DeepFoxxx <151031851+HighwayStarr@users.noreply.github.com>
2024-11-27 20:49:48 +0600

    Update bot.ts

--- lib/bot.ts
+++ lib/bot.ts
@@ -1,30 +1,20 @@
-import { Bot, InlineKeyboard } from "https://deno.land/x/grammy@v1.32.0/mod.ts";
+import { Bot } from "https://deno.land/x/telegraf/mod.ts";  
 
-// Создайте экземпляр класса `Bot` и передайте ему токен вашего бота.
-// Токен и адрес бэкенда мы спрячем, чтобы никто не смог воспользоваться нашим ботом или взломать нас. Получим их из файла .env (или из настроек в Deno Deploy)
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "8142066967:AAE8p2Zn4ejTvzoPb1HPjlYV6ZuCrECFmVU"); // export нужен, чтобы воспользоваться ботом в другом файле
+// Замените <ваш_токен> на токен вашего бота  
+const bot = new Bot("<ваш_токен>");  
 
-// Теперь вы можете зарегистрировать слушателей на объекте вашего бота `bot`.
-// grammY будет вызывать слушателей, когда пользователи будут отправлять сообщения вашему боту.
+bot.start((ctx) => {  
+  ctx.reply("Привет! Я ваш новый бот. Как я могу помочь?");  
+});  
 
-// Обработайте команду /start.
-bot.command(
-    "start",
-    (ctx) => ctx.reply("Добро пожаловать. Запущен и работает!",{ reply_markup: keyboard }),
-);
+bot.help((ctx) => {  
+  ctx.reply("Список доступных команд:\n/start - начать\n/help - получить помощь");  
+});  
 
-// Обработайте другие сообщения.
-bot.on("message", (ctx) => ctx.reply("Получил ваше сообщение: " + ctx.message.text + " !",));
+// Эхо-ответ на текстовые сообщения  
+bot.on("text", (ctx) => {  
+  ctx.reply(`Вы написали: ${ctx.message.text}`);  
+});  
 
-// Клавиатура будет отправлять в бота команду /about
-const keyboard = new InlineKeyboard()
-    .text("Обо мне", "/about");
-
-bot.callbackQuery("/about", async (ctx) => {
-    await ctx.answerCallbackQuery(); // Уведомляем Telegram, что мы обработали запрос
-    await ctx.reply("Я бот? Я бот... Я Бот!");
-});
-
-// Теперь, когда вы указали, как обрабатывать сообщения, вы можете запустить своего бота.
-// Он подключится к серверам Telegram и будет ждать сообщений.
-// Запуск бота производится из файла main.ts
+// Запуск бота  
+bot.launch();
