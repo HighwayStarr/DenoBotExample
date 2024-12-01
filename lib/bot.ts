3624fcef44451c295dac27fda78a24c7a778a4ca
DeepFoxxx <151031851+HighwayStarr@users.noreply.github.com>
2024-12-01 11:47:38 +0600

    Update bot.ts

--- lib/bot.ts
+++ lib/bot.ts
@@ -8,19 +8,19 @@ export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "8142066967:AAE8p2Zn4ejT
 // Обработайте команду /start.
 bot.command(    
     "start",
-    (ctx) => ctx.reply("Добро пожаловать. Запущен и работает!",{ reply_markup: keyboard }),
+    (ctx) => ctx.reply("ку полина лузарева",{ reply_markup: keyboard }),
 );
 
 // Обработайте другие сообщения.
-bot.on("message", (ctx) => ctx.reply("Получил ваше сообщение: " + ctx.message.text + " !",));
+bot.on("message", (ctx) => ctx.reply("и тебе" + ctx.message.text,));
 
 // Клавиатура будет отправлять в бота команду /about
 const keyboard = new InlineKeyboard()
-    .text("Обо мне", "/about");
+    .text("нажми если любиш егора", "/about");
 
 bot.callbackQuery("/about", async (ctx) => {    
     await ctx.answerCallbackQuery(); // Уведомляем Telegram, что мы обработали запрос
-    await ctx.reply("Я бот? Я бот... Я Бот!");});
+    await ctx.reply("ты шо туп");});
 
 // Теперь, когда вы указали, как обрабатывать сообщения, вы можете запустить своего бота.
 // Он подключится к серверам Telegram и будет ждать сообщений.// Запуск бота производится из файла main.ts
